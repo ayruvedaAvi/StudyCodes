c8178906e79d3be857845e156e4b37fe3447394d
Abhishek <neupaneabhishek13@gmail.com>
2023-03-29 22:47:54 +0545

    minor change

--- Java /Old questions /2021 fall/check.java
+++ Java /Old questions /2021 fall/check.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
-class Ashutosh{
-    String temp;
-    int n;
-    Scanner scan=new Scanner(System.in);
-    while(n=0){
-        //Value enter code
+// import java.util.Scanner;
+// class Ashutosh{
+//     String temp;
+//     int n;
+//     Scanner scan=new Scanner(System.in);
+//     while(n=0){
+//         //Value enter code
 
-        //
-        System.out.println("Press N to exit.");
-        temp=scan.nextLine();
-        if(temp.equalsIgnoreCase("N")){
-            n=2;
-        }
-    }
-}
+//         //
+//         System.out.println("Press N to exit.");
+//         temp=scan.nextLine();
+//         if(temp.equalsIgnoreCase("N")){
+//             n=2;
+//         }
+//     }
+// }
